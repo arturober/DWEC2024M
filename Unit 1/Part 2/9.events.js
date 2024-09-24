c8bb8a9f8ff56a490e48c29951c8b5a3d3daa72f
Arturo Bernal <arturober@gmail.com>
2024-09-24 08:32:04 +0200

    events

---
+++ Unit 1/Part 2/9.events.js
@@ -0,0 +1,12 @@
+function onClick(e) {
+    console.log(`Event ${e.type} on element ${this.id}`);
+}
+
+const btn1 = document.getElementById("btn1");
+// btn1.addEventListener('click', function(e) {
+//     console.log(`Event ${e.type} on element ${this.id}`)
+// });
+// btn1.addEventListener('click', (e) => {
+//     console.log(`Event ${e.type} on element ${e.target.id}`)
+// });
+btn1.addEventListener('click', onClick);
