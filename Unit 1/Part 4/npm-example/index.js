3b714a92d9437ee9034d7dc430d2ea0726eaa284
Arturo Bernal <arturober@gmail.com>
2024-10-07 09:47:06 +0200

    07/10

---
+++ Unit 1/Part 4/npm-example/index.js
@@ -0,0 +1,11 @@
+import "./node_modules/dayjs/dayjs.min.js";
+
+console.log("Hello world!");
+console.log(dayjs('2023-08-25').format('DD/MM/YYYY')); // '25/08/2023'
+
+fe(34);
+function hello() {
+    console.log("Hello");
+}
+
+hello();
