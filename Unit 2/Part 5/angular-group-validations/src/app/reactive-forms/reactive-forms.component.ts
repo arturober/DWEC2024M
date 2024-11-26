a033017af7942175b6232b70af6ca0af9ba9921f
Arturo Bernal <arturo@email.com>
2024-11-26 23:16:31 +0100

    form gorup validations examples

---
+++ Unit 2/Part 5/angular-group-validations/src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reactive-forms',
+  imports: [],
+  templateUrl: './reactive-forms.component.html',
+  styleUrl: './reactive-forms.component.css'
+})
+export class ReactiveFormsComponent {
+
+}
