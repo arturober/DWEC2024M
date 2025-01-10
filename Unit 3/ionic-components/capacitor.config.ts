1f1b65206935694ff202ebe4cac23469518f0385
Arturo Bernal <arturober@gmail.com>
2025-01-10 09:37:46 +0100

    android

--- Unit 3/ionic-components/capacitor.config.ts
+++ Unit 3/ionic-components/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'dwec.ionic.components',
   appName: 'Ionic Components',
-  webDir: 'www'
+  webDir: 'www/browser'
 };
 
 export default config;
